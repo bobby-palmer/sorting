3fd5914aa73470bdaa6c45a05b23f36d35a637f7
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-09-30 14:39:05 -0400

    implmement bubble sort

---
+++ sort.h
@@ -0,0 +1,14 @@
+#include <cstddef>
+inline void bubbleSort(int arr[], size_t size) {
+  size_t sorted = 0;
+  while (sorted < size) {
+    for (size_t i = size - 1; i > 0; ++i ) {
+      if(arr[i] < arr[i - 1]) {
+        int temp = arr[i];
+        arr[i] = arr[i - 1];
+        arr[i - 1] = temp;
+      }
+    }
+    sorted++;
+  }
+}; // bubbleSort()
